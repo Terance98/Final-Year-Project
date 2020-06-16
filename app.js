000dd902e1ccd93c0a62daea70d69bf9b1aad3fd
Thomas Terance <terance@pop-os.localdomain>
2020-06-17 00:20:18 +0530

    Project Finished

--- app.js
+++ app.js
@@ -15,21 +15,19 @@ const TwilioClient = require('twilio')(accountSid, authToken);
 var nodemailer = require('nodemailer');
 var sgTransport = require('nodemailer-sendgrid-transport');
 
-var options = {
-    // service: 'SendGrid',
-    auth: {
-        // api_user: 'apikey',
+var options = { // service: 'SendGrid',
+    auth: { // api_user: 'apikey',
         api_key: process.env.SENDGRID_KEY
     }
 }
 
 var EmailClient = nodemailer.createTransport(sgTransport(options));
 // const Bluebird = require('bluebird');
-//hello
+// hello
 // hai
 // fetch.Promise = Bluebird;
 
-const { URLSearchParams } = require('url');
+const {URLSearchParams} = require('url');
 // Make a request for a user with a given ID
 
 var app = express();
@@ -40,10 +38,10 @@ app.use(express.static("match"));
 
 app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
 // const directoryPath = path.join(__dirname, '/uploads');
 
-//Code to get the no.of files in a directory
+// Code to get the no.of files in a directory
 // function dirList(path) {
 //     return new Promise((resolve, reject) => {
 //         fs.readdir(path, function (err, files) {
@@ -55,20 +53,20 @@ mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
 // }
 app.use(cookieParser());
 app.use(upload()); // configure middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
-//Mongoose Schemas
+// Mongoose Schemas
 // const Child = mongoose.model('Child', { name: String, image_ID: String });
 
-//Mongoose model for children
+// Mongoose model for children
 const Schema = mongoose.Schema;
 
 var UserSchema = new mongoose.Schema({
     username: String,
     email: String,
     phone: String,
-    password: String,
-}, { timestamps: true });
+    password: String
+}, {timestamps: true});
 
 const User = mongoose.model('User', UserSchema);
 
@@ -80,68 +78,67 @@ const User = mongoose.model('User', UserSchema);
 
 // const Child = mongoose.model('Child', ChildSchema);
 
-const Missing = mongoose.model('Missing',
-    new Schema({}),
-    'missing');
+const Missing = mongoose.model('Missing', new Schema({}), 'missing');
 
-function tConvert(time) {
-    // Check correct time format and split into components
+function tConvert(time) { // Check correct time format and split into components
     time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
 
     if (time.length > 1) { // If time format correct
-        time = time.slice(1);  // Remove full string match value
-        time[5] = +time[0] < 12 ? ' AM' : ' PM'; // Set AM/PM
-        time[0] = +time[0] % 12 || 12; // Adjust hours
+        time = time.slice(1); // Remove full string match value
+        time[5] = + time[0] < 12 ? ' AM' : ' PM'; // Set AM/PM
+        time[0] = + time[0] % 12 || 12; // Adjust hours
     }
     return time.join(''); // return adjusted time or original string
-}
-
-app.get('/', function (req, res) {
+}app.get('/', function (req, res) {
     const userID = req.cookies.userID;
-    if (!userID) return res.redirect("/signin");
+    if (! userID) 
+        return res.redirect("/signin");
+    
     res.render('index');
 });
 
 app.post('/upload', function (req, res) {
     const userID = req.cookies.userID;
-    if (!userID) return res.redirect("/signin");
-    console.log(req.files);
+    if (! userID) 
+        return res.redirect("/signin");
+    
 })
 
 app.get("/report", function (req, res) {
     const userID = req.cookies.userID;
-    if (!userID) return res.redirect("/signin");
+    if (! userID) 
+        return res.redirect("/signin");
+    
 
     res.render('report');
 });
 
 
 app.post("/report", function (req, res) {
-    console.log(req.body);
     const userID = req.cookies.userID;
-    if (!userID) return res.redirect("/signin");
+    if (! userID) 
+        return res.redirect("/signin");
+    
 
     try {
         const childName = req.body.childName;
         const files = [];
         if (Array.isArray(req.files.upfile)) {
-            files.push(...req.files.upfile);
+            files.push(... req.files.upfile);
         } else {
             files.push(req.files.upfile);
         }
         if (files.length) {
-            files.forEach((item, index) => {
-                //use uuid instead of childName
+            files.forEach((item, index) => { // use uuid instead of childName
                 let fileName = _.split(childName, ' ')[0] + (index + 1) + ".jpg";
                 let uploadpath = __dirname + '/match/' + fileName;
 
                 item.mv(uploadpath, function (err) {
                     if (err) {
                         console.log("File Upload Failed", fileName, err);
-                    }
-                    else {
+                    } else {
                         console.log("File Uploaded", fileName);
-                        //Instead of writing to db here, pass it on to the python API and handle data insertion from there.
+                        // Instead of writing to db here, pass it on to the python API and handle data insertion from there.
                         // const childData = new Child({ name: childName, image_ID: fileName, parent: userID });
                         // console.log(childData);
                         // childData.save().then(() => console.log("Child's data is written!"));
@@ -156,19 +153,19 @@ app.post("/report", function (req, res) {
             // params.append('name', childName);
 
             const fetchUrl = "http://127.0.0.1:5000/";
-            fetch(fetchUrl, { method: 'POST', body: params })
-                .then(res => res.json())
-                .then(result => {
-                    console.log(result);
-                    if (result['status']) {
-                        return res.render("after_report")
-                    }
-                    return res.send("<h1>Training unsuccessful</h1>");
-                })
-                .catch(err => {
-                    console.log("API error - ", err);
-                    return res.redirect("/report");
-                })
+            fetch(fetchUrl, {
+                method: 'POST',
+                body: params
+            }).then(res => res.json()).then(result => {
+                console.log(result);
+                if (result['status']) {
+                    return res.render("after_report")
+                }
+                return res.send("<h1>Training unsuccessful</h1>");
+            }).catch(err => {
+                console.log("API error - ", err);
+                return res.redirect("/report");
+            })
         } else {
             res.send("No File selected !");
             res.end();
@@ -181,15 +178,21 @@ app.post("/report", function (req, res) {
 
 app.get("/find", function (req, res) {
     const userID = req.cookies.userID;
-    if (!userID) return res.redirect("/signin");
+    if (! userID) 
+        return res.redirect("/signin");
+    
 
     res.render('find');
 });
 
 app.post("/find", function (req, res) {
     const userID = req.cookies.userID;
-    if (!userID) return res.redirect("/signin");
-    if (!req.body.lat || !req.body.long) return res.redirect("/find");
+    if (! userID) 
+        return res.redirect("/signin");
+    
+    if (! req.body.lat || ! req.body.long) 
+        return res.redirect("/find");
+    
     try {
         if (req.files.upfile) {
             const name = "suspicious.jpg";
@@ -205,72 +208,81 @@ app.post("/find", function (req, res) {
             const params = new URLSearchParams();
 
             const fetchUrl = "http://127.0.0.1:6000/";
-            fetch(fetchUrl, { method: 'POST', body: params })
-                .then(res => res.json())
-                .then(async results => {
-                    if (results.output.length) {
-                        const childPersonId = results.output[0].person_id;
+            fetch(fetchUrl, {
+                method: 'POST',
+                body: params
+            }).then(res => res.json()).then(async results => {
+                if (results.output.length) {
+                    const childPersonId = results.output[0].person_id;
 
-                        const childData = await Missing.findOne({ azure_face_id: childPersonId });
-                        const reportedUser = await User.findOne({ _id: userID });
-                        // const childName = childData.name;
-                        const { name: childName, email: guardianEmail, phone: guardianPhone } = JSON.parse(JSON.stringify(childData));
-                        const { username: reportedUserName, email: reportedUserEmail, phone: reportedUserPhone } = reportedUser;
-                        const childSpottedLocationUrl = `http://maps.google.com/?q=${req.body.lat},${req.body.long}`;
-                        console.log(guardianPhone);
-                        // For todays date;
-                        Date.prototype.today = function () {
-                            return ((this.getDate() < 10) ? "0" : "") + this.getDate() + "/" + (((this.getMonth() + 1) < 10) ? "0" : "") + (this.getMonth() + 1) + "/" + this.getFullYear();
-                        }
+                    const childData = await Missing.findOne({azure_face_id: childPersonId});
+                    const reportedUser = await User.findOne({_id: userID});
+                    // const childName = childData.name;
+                    let {name: childName, email: guardianEmail, phone: guardianPhone} = JSON.parse(JSON.stringify(childData));
+                    const {username: reportedUserName, email: reportedUserEmail, phone: reportedUserPhone} = reportedUser;
+                    const childSpottedLocationUrl = `http://maps.google.com/?q=${
+                        req.body.lat
+                    },${
+                        req.body.long
+                    }`;
 
-                        // For the time now
-                        Date.prototype.timeNow = function () {
-                            return ((this.getHours() < 10) ? "0" : "") + this.getHours() + ":" + ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + ":" + ((this.getSeconds() < 10) ? "0" : "") + this.getSeconds();
-                        }
-
-                        const dateTime = new Date().today() + ' at ' + tConvert(new Date().timeNow());
-                        // const guardianEmail = "thomasterance2020@cs.ajce.in";
-                        // const guardianPhone = "+919496342920";
-                        const foundChildText = `Your child ${childName} was found on ${dateTime}. Here are the details of the person who found your child,\n\nName - ${reportedUserName}\nEmail - ${reportedUserEmail}\nPhone - ${reportedUserPhone}\n\nClick the link to get the spotted location : ${childSpottedLocationUrl}`;
-                        // console.log(foundChildText);
-                        const email = {
-                            from: 'thomasterance98@gmail.com',
-                            to: guardianEmail,
-                            subject: 'Your child was found',
-                            text: foundChildText
-                        };
-
-                        //Send an email
-                        EmailClient.sendMail(email, function (err, info) {
-                            if (err) {
-                                console.log(err);
-                            }
-                            else {
-                                console.log('Email sent!');
-                            }
-                        });
-
-                        //Send an sms
-                        TwilioClient.messages
-                            .create({
-                                body: foundChildText,
-                                from: '+16123954705',
-                                to: guardianPhone
-                            })
-                            .then(message => console.log('Message sent!'))
-                            .catch(err => console.log(err));
-
-                        res.render("after_find", results);
-
-                        //Also send mail and sms to the user who found the child
-                    } else {
-                        console.log('No match found!');
-                        throw Error;
+                    if (!guardianPhone) 
+                        guardianPhone = "+919496342920";
+                    
+                    if (!guardianEmail) 
+                        guardianEmail = "thomasterance2020@cs.ajce.in";
+                    
+                    // For todays date;
+                    Date.prototype.today = function () {
+                        return((this.getDate() < 10) ? "0" : "") + this.getDate() + "/" + (
+                        ((this.getMonth() + 1) < 10) ? "0" : ""
+                    ) + (this.getMonth() + 1) + "/" + this.getFullYear();
                     }
-                }).catch(err => {
-                    console.log(err);
-                    res.render("cannot_find_error");
-                })
+
+                    // For the time now
+                    Date.prototype.timeNow = function () {
+                        return((this.getHours() < 10) ? "0" : "") + this.getHours() + ":" + (
+                        (this.getMinutes() < 10) ? "0" : ""
+                    ) + this.getMinutes() + ":" + (
+                        (this.getSeconds() < 10) ? "0" : ""
+                    ) + this.getSeconds();
+                    }
+
+                    const dateTime = new Date().today() + ' at ' + tConvert(new Date().timeNow());
+                    // const guardianEmail = "thomasterance2020@cs.ajce.in";
+                    // const guardianPhone = "+919496342920";
+                    const foundChildText = `Your child ${childName} was found on ${dateTime}. Here are the details of the person who found your child,\n\nName - ${reportedUserName}\nEmail - ${reportedUserEmail}\nPhone - ${reportedUserPhone}\n\nClick the link to get the spotted location : ${childSpottedLocationUrl}`;
+                    // console.log(foundChildText);
+                    const email = {
+                        from: 'thomasterance98@gmail.com',
+                        to: guardianEmail,
+                        subject: 'Your child was found',
+                        text: foundChildText
+                    };
+
+                    // Send an email
+                    EmailClient.sendMail(email, function (err, info) {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log('Email sent!');
+                        }
+                    });
+
+                    // Send an sms
+                    TwilioClient.messages.create({body: foundChildText, from: '+16123954705', to: guardianPhone}).then(message => console.log('Message sent!')).catch(err => console.log(err));
+
+                    res.render("after_find", results);
+
+                    // Also send mail and sms to the user who found the child
+                } else {
+                    console.log('No match found!');
+                    throw Error;
+                }
+            }).catch(err => {
+                console.log(err);
+                res.render("cannot_find_error");
+            })
 
         } else {
             res.send("No File selected !");
@@ -285,37 +297,44 @@ app.post("/find", function (req, res) {
 
 app.get("/afterfind", (req, res) => {
     const userID = req.cookies.userID;
-    if (!userID) return res.redirect("/signin");
+    if (! userID) 
+        return res.redirect("/signin");
+    
 
     res.render("after_find");
 });
 
 app.get("/afterreport", (req, res) => {
     const userID = req.cookies.userID;
-    if (!userID) return res.redirect("/signin");
+    if (! userID) 
+        return res.redirect("/signin");
+    
 
     res.render("after_report");
 });
 
 app.get('/signin', (req, res) => {
     const userID = req.cookies.userID;
-    if (userID) return res.redirect("/");
+    if (userID) 
+        return res.redirect("/");
+    
 
     res.render('signIn');
 });
 
 app.post('/signin', async (req, res) => {
     const userID = req.cookies.userID;
-    if (userID) return res.redirect("/");
+    if (userID) 
+        return res.redirect("/");
+    
 
     try {
         const email = req.body.email;
         const password = req.body.password;
 
-        const ifUserExists = await User.findOne({ email: email, password: password });
-        if (ifUserExists) {
-            //Setting the cookie value
-            res.cookie('userID', ifUserExists._id, { maxAge: 3600000 }).redirect('/')
+        const ifUserExists = await User.findOne({email: email, password: password});
+        if (ifUserExists) { // Setting the cookie value
+            res.cookie('userID', ifUserExists._id, {maxAge: 3600000}).redirect('/')
         } else {
             return res.redirect("/signin");
         }
@@ -328,14 +347,18 @@ app.post('/signin', async (req, res) => {
 
 app.get('/signup', (req, res) => {
     const userID = req.cookies.userID;
-    if (userID) return res.redirect("/");
+    if (userID) 
+        return res.redirect("/");
+    
 
     res.render('signUp');
 });
 
 app.post('/signup', async (req, res) => {
     const userID = req.cookies.userID;
-    if (userID) return res.redirect("/");
+    if (userID) 
+        return res.redirect("/");
+    
 
     try {
         const firstName = req.body.first_name;
@@ -345,9 +368,22 @@ app.post('/signup', async (req, res) => {
         const password = req.body.password;
         const confirmPassword = req.body.confirmPassword;
 
-        [firstName, lastName, email, password, phone, confirmPassword].forEach(item => { if (!item) return res.redirect("/signup") })
+        [
+            firstName,
+            lastName,
+            email,
+            password,
+            phone,
+            confirmPassword
+        ].forEach(item => {
+            if (!item) 
+                return res.redirect("/signup")
+            
+        })
 
-        if (confirmPassword !== password) return res.redirect("/signup");
+        if (confirmPassword !== password) 
+            return res.redirect("/signup");
+        
 
         const userData = {
             username: firstName + ' ' + lastName,
@@ -356,13 +392,13 @@ app.post('/signup', async (req, res) => {
             password: password
         }
 
-        const ifUserExists = await User.findOne({ email: email });
-        console.log(ifUserExists);
-        if (ifUserExists) return res.redirect("/signup");
+        const ifUserExists = await User.findOne({email: email});
+        if (ifUserExists) 
+            return res.redirect("/signup");
+        
 
         const user = new User(userData);
         const response = await user.save();
-        console.log(response);
         res.redirect('/');
     } catch (err) {
         console.log(err);
@@ -374,10 +410,10 @@ app.get("/signout", (req, res) => res.clearCookie('userID').redirect("/"))
 
 app.get('/get-details/:personId', async (req, res) => {
     const personId = req.params.personId;
-    console.log(personId);
     try {
-        const childDetails = await Missing.findOne({ azure_face_id: personId });
-        console.log(childDetails);
+        const childDetails = JSON.parse(JSON.stringify(await Missing.findOne({azure_face_id: personId})));
+        childDetails.fname = childDetails.name.split(" ")[0];
+        res.render('details', {childDetails});
 
 
     } catch (err) {
